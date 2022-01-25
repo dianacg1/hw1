202db7c18a33d7d0739b5c0220bfd148ec1a82b8
dianacg1 <diana.carreno@kellogg.northwestern.edu>
2022-01-25 17:29:23 +0000

    Merged first and last name

--- kmdb.sql
+++ kmdb.sql
@@ -2,7 +2,6 @@
 -- structure, and data for "KMDB" (the Kellogg Movie Database).
 -- The end product will be a report that prints the movies and the
 -- top-billed cast for each movie in the database.
- 
 -- Requirements/assumptions
 --
 -- - There will only be three movies in the database – the three films
@@ -12,7 +11,6 @@
 -- - A movie has a single director
 -- - A person can be the director of and/or play a role in a movie
 -- - Everything you need to do in this assignment is marked with TODO!
- 
 -- Rubric
 --
 -- There are three deliverables for this assignment, all delivered via
@@ -24,7 +22,6 @@
 -- - Insertion of "Batman" sample data into tables (5 points)
 -- - Selection of data, so that something similar to the following sample
 --   "report" can be achieved (5 points)
- 
 -- Submission
 --
 -- - "Use this template" to create a brand-new "hw1" repository in your
@@ -33,19 +30,14 @@
 -- - When done, commit and sync a final time, before submitting the GitHub
 --   URL for the finished "hw1" repository as the "Website URL" for the
 --   Homework 1 assignment in Canvas
- 
 -- Successful sample output is as shown:
- 
 -- Movies
 -- ======
- 
 -- Batman Begins          2005           PG-13  Christopher Nolan
 -- The Dark Knight        2008           PG-13  Christopher Nolan
 -- The Dark Knight Rises  2012           PG-13  Christopher Nolan
- 
 -- Top Cast
 -- ========
- 
 -- Batman Begins          Christian Bale        Bruce Wayne
 -- Batman Begins          Michael Caine         Alfred
 -- Batman Begins          Liam Neeson           Ra's Al Ghul
@@ -61,18 +53,19 @@
 -- The Dark Knight Rises  Tom Hardy             Bane
 -- The Dark Knight Rises  Joseph Gordon-Levitt  John Blake
 -- The Dark Knight Rises  Anne Hathaway         Selina Kyle
- 
 -- Turns column mode on but headers off
 .mode column
 .headers off
- 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO! DROP TABLE
+ 
 DROP TABLE IF EXISTS Movies;
 DROP TABLE IF EXISTS Top_Cast;
 DROP TABLE IF EXISTS Directors;
+ 
 -- Create new tables, according to your domain model
 -- TODO!
+ 
 CREATE TABLE Movies (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 title TEXT,
@@ -81,268 +74,236 @@ mpaa_rating TEXT
 );
  
 CREATE TABLE Top_Cast (
-   id INTEGER PRIMARY KEY AUTOINCREMENT,
-   first_name TEXT,
-   last_name TEXT,
-   character TEXT,
-   movies_id INTEGER
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  character TEXT,
+  movies_id INTEGER
 );
  
 CREATE TABLE Directors (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-first_name TEXT,
-last_name TEXT,
+name TEXT,
 movies_id INTEGER
 );
+ 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+ 
 INSERT INTO Movies (
-  title,
-  year_released,
-  mpaa_rating
+ title,
+ year_released,
+ mpaa_rating
 ) VALUES (
-  "Batman Begins",
-  2005,
-  "PG-13"
+ "Batman Begins",
+ 2005,
+ "PG-13"
 );
+ 
 INSERT INTO Movies (
-  title,
-  year_released,
-  mpaa_rating
+ title,
+ year_released,
+ mpaa_rating
 ) VALUES (
-  "The Dark Knight",
-  2008,
-  "PG-13"
+ "The Dark Knight",
+ 2008,
+ "PG-13"
 );
+ 
 INSERT INTO Movies (
-  title,
-  year_released,
-  mpaa_rating
+ title,
+ year_released,
+ mpaa_rating
 ) VALUES (
-  "The Dark Knight Rises",
-  2012,
-  "PG-13"
+ "The Dark Knight Rises",
+ 2012,
+ "PG-13"
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Christian",
-  "Bale",
-  "Bruce Wayne",
-  1
+ "Christian Bale",
+ "Bruce Wayne",
+ 1
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Michael",
-  "Caine",
-  "Alfred",
-  1
+ "Michael Caine",
+ "Alfred",
+ 1
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name, 
+ character,
+ movies_id
 ) VALUES (
-  "Liam",
-  "Neeson",
-  "Ra's Al Ghul",
-  1
+ "Liam Neeson",
+ "Ra's Al Ghul",
+ 1
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Katie",
-  "Holmes",
-  "Rachel Dawes",
-  1
+ "Katie Holmes",
+ "Rachel Dawes",
+ 1
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Gary",
-  "Oldman",
-  "Commissioner Gordon",
-  1
+ "Gary Oldman",
+ "Commissioner Gordon",
+ 1
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Christian",
-  "Bale",
-  "Bruce Wayne",
-  2
+ "Christian Bale",
+ "Bruce Wayne",
+ 2
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Heath",
-  "Ledger",
-  "Joker",
-  2
+ "Heath Ledger",
+ "Joker",
+ 2
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Aaron",
-  "Eckhart",
-  "Harvey Dent",
-  2
+ "Aaron Eckhart",
+ "Harvey Dent",
+ 2
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Michael",
-  "Caine",
-  "Alfred",
-  2
+ "Michael Caine",
+ "Alfred",
+ 2
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name, 
+ character,
+ movies_id
 ) VALUES (
-  "Maggie",
-  "Gyllenhaal",
-  "Rachel Dawes",
-  2
+ "Maggie Gyllenhaal",
+ "Rachel Dawes",
+ 2
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Christian",
-  "Bale",
-  "Bruce Wayne",
-  3
+ "Christian Bale",
+ "Bruce Wayne",
+ 3
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Gary",
-  "Oldman",
-  "Commissioner Gordon",
-  3
+ "Gary Oldman",
+ "Commissioner Gordon",
+ 3
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Tom",
-  "Hardy",
-  "Bane",
-  3
+ "Tom Hardy",
+ "Bane",
+ 3
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Joseph",
-  "Gordon-Levitt",
-  "John Blake",
-  3
+ "Joseph",
+ "Gordon-Levitt",
+ "John Blake",
+ 3
 );
  
 INSERT INTO Top_Cast (
-  first_name,
-  last_name,
-  character,
-  movies_id
+ name,
+ character,
+ movies_id
 ) VALUES (
-  "Anne",
-  "Hathaway",
-  "Selina Kyle",
-  3
+ "Anne Hathaway",
+ "Selina Kyle",
+ 3
 );
  
 INSERT INTO Directors (
-   first_name,
-   last_name,
-   movies_id
+  name,
+  movies_id
 ) VALUES (
-  "Christopher",
-  "Nolan",
-  1
+ "Christopher Nolan",
+ 1
 );
  
 INSERT INTO Directors (
-   first_name,
-   last_name,
-   movies_id
+  name,
+  movies_id
 ) VALUES (
-  "Christopher",
-  "Nolan",
-  2
+ "Christopher Nolan",
+ 2
 );
  
 INSERT INTO Directors (
-   first_name,
-   last_name,
-   movies_id
+  name,
+  movies_id
 ) VALUES (
-  "Christopher",
-  "Nolan",
-  3
+ "Christopher Nolan",
+ 3
 );
+ 
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
 .print ""
-
+ 
 -- The SQL statement for the movies output
 -- TODO!
  
-SELECT Movies.title, Movies.year_released, Movies.mpaa_rating, Directors.first_name, Directors.last_name
+SELECT Movies.title, Movies.year_released, Movies.mpaa_rating, Directors.name
 FROM Movies INNER JOIN Directors ON Movies.id = Directors.movies_id;
  
 -- Prints a header for the Top_Cast output
@@ -350,8 +311,9 @@ FROM Movies INNER JOIN Directors ON Movies.id = Directors.movies_id;
 .print "Top Cast"
 .print "========"
 .print ""
+ 
 -- The SQL statement for the Top_Cast output
 -- TODO!
  
-SELECT Movies.title, (Top_Cast.first_name||" "||Top_Cast.last_name) as actor, Top_Cast.character
+SELECT Movies.title, Top_Cast.name, Top_Cast.character
 FROM Movies INNER JOIN Top_Cast ON Movies.id = Top_Cast.movies_id;
